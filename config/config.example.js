71a4f8926ddbe6d8535a303660de03cc7bc9b655
WookashWackomy <wookie.xp.07@gmail.com>
2019-06-26 00:18:02 +0200

    configure server

--- config/config.example.js
+++ config/config.example.js
@@ -1,4 +1,4 @@
-// Copy this file as config.js in the same folder, with the proper database connection URI.
+//Copy this file as config.js in the same folder, with the proper database connection URI.
 
 module.exports = {
   db: 'mongodb://username:password@url:port/db',
