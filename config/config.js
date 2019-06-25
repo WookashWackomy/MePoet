ccfaf932fad2a2c23c90bee4518cbb29b5152d1f
WookashWackomy <wookie.xp.07@gmail.com>
2019-06-26 00:59:24 +0200

    further configuration of webpack

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 //Copy this file as config.js in the same folder, with the proper database connection URI.
 
 module.exports = {
-  db: 'mongodb://heroku_kd5nkj02:6fst1u2ijh2mbo6lg0irclrrpl@ds243607.mlab.com:43607/heroku_kd5nkj02',
+  db: 'mongodb://first:Firstuser1@ds243607.mlab.com:43607/heroku_kd5nkj02',
   db_dev: 'mongodb://localhost:27017/db',
 };
