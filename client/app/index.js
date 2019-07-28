931778565b22b4736cdd9ca22da919bef5f6ffd8
WookashWackomy <wookie.xp.07@gmail.com>
2019-07-28 13:37:46 +0200

    fix routing

--- client/app/index.js
+++ client/app/index.js
@@ -1,35 +1,32 @@
-import React from 'react';
-import { render } from 'react-dom';
+import React from "react";
+import { render } from "react-dom";
 
-import {
-  BrowserRouter as Router,
-  Route,
-  Link,
-  Switch
-} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
-import App from './components/App/App';
-import NotFound from './components/App/NotFound';
+import App from "./components/App/App";
+import NotFound from "./components/App/NotFound";
 
-import Home from './components/Home/Home';
+import Home from "./components/Home/Home";
 
-import HelloWorld from './components/HelloWorld/HelloWorld';
+import HelloWorld from "./components/HelloWorld/HelloWorld";
 
-import './styles/styles.scss';
+import "./styles/styles.scss";
 import ComposePoem from "./components/ComposePoem/ComposePoem";
 import Login from "./components/Login/Login";
 //import 'typeface-roboto';
 
-render((
+render(
   <Router>
     <App>
       <Switch>
-        <Route exact path="/" component={Home}/>
-        <Route path="/helloworld" component={HelloWorld}/>
-        <Route path="/composePoem" component={ComposePoem}/>
-        <Route path="/login" component={Login}/>
-        <Route component={NotFound}/>
+        <Route exact path="/" component={Home}>
+          <Route path="helloworld" component={HelloWorld} />
+          <Route path="composePoem" component={ComposePoem} />
+          <Route path="login" component={Login} />
+          <Route component={NotFound} />
+        </Route>
       </Switch>
     </App>
-  </Router>
-), document.getElementById('app'));
+  </Router>,
+  document.getElementById("app")
+);
--- server/server.js
+++ server/server.js
@@ -62,9 +62,9 @@ if (isDev) {
   app.use(webpackHotMiddleware(compiler));
   app.use(express.static(path.resolve(__dirname, "../dist")));
 } else {
-  app.use(express.static(path.resolve(__dirname, "../build")));
+  app.use(express.static(path.resolve(__dirname, "../dist")));
   app.get("*", function(req, res) {
-    res.sendFile(path.resolve(__dirname, "../build/index.html"));
+    res.sendFile(path.resolve(__dirname, "../dist/index.html"));
     res.end();
   });
 }
