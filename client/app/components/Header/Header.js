814a768203074b3a4990aab16cd744b10d5325a5
WookashWackomy <wookie.xp.07@gmail.com>
2019-07-28 13:23:41 +0200

    add links to navbar

--- client/app/components/Header/Header.js
+++ client/app/components/Header/Header.js
@@ -1,18 +1,27 @@
-import React from 'react';
+import React from "react";
 
-import {Link} from 'react-router-dom';
-import logo from '../../../public/assets/img/logo.png';
+import { Link } from "react-router-dom";
+import logo from "../../../public/assets/img/logo.png";
 
 const Header = () => (
   <header>
-    <Link to="/">Home</Link>
-
-    <nav className="navbar navbar-dark bg-dark-green" style={{marginBottom: "20px"}}>
-      <Link to="/" className="navbar-brand navbar-left mb-0 h1" style={{color: "white", width: "100%"}}><img
-        src={logo} alt={"logo"}/></Link>
+    <nav
+      className="navbar navbar-dark bg-dark-green"
+      style={{ marginBottom: "20px" }}
+    >
+      <Link
+        to="/"
+        className="navbar-brand navbar-left mb-0 h1"
+        style={{ color: "white", width: "100%" }}
+      >
+        <img src={logo} alt={"logo"} />
+      </Link>
+      <Link to="/">Home</Link>
+      <Link to="/login">login</Link>
+      <Link to="/composePoem">Compose!</Link>
     </nav>
 
-    <hr/>
+    <hr />
   </header>
 );
 
