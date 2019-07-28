8c31b8b32c3ec643a203c2b58d2a8b0defcca8f6
WookashWackomy <wookie.xp.07@gmail.com>
2019-07-28 13:34:20 +0200

    add links to navbar

--- server/server.js
+++ server/server.js
@@ -62,9 +62,9 @@ if (isDev) {
   app.use(webpackHotMiddleware(compiler));
   app.use(express.static(path.resolve(__dirname, "../dist")));
 } else {
-  app.use(express.static(path.resolve(__dirname, "../dist")));
+  app.use(express.static(path.resolve(__dirname, "../build")));
   app.get("*", function(req, res) {
-    res.sendFile(path.resolve(__dirname, "../dist/index.html"));
+    res.sendFile(path.resolve(__dirname, "../build/index.html"));
     res.end();
   });
 }
